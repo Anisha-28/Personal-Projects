97361e964f0e602ea65d372f6e4bc1c5ec039c98
Anisha-28 <anisha.a.2812@gmail.com>
2020-09-11 13:42:44 +1000

    Tutorial 1: Few programs made while course

---
+++ W1sol.java
@@ -0,0 +1,18 @@
+
+public class JRE {
+
+	public static void main(String[] args) {
+		// The code below prints a brief introduction.
+		System.out.println("Hello World");
+		System.out.println(" Anisha , Thuirsday , 8:30a.m. , 14.09.23 , Coding ");
+		
+		// Box a. It should be println but it has printIn 
+		// Box b. The initial of the word System is in small letter's' and it should be in capital letter'S'
+		// Box c. It doesn't have the " Box c. has " missing after Hello World
+		// Box d. It doesn't have { after the library public class HelloWorld 
+		// Box d. also it has void static in library and it should be static void
+		
+		
+	}
+
+}
